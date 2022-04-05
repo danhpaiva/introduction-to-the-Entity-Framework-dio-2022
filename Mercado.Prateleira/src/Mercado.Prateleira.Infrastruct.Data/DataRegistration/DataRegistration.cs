2d76b1c64e08d1a7e97f48b4f6ac3b6a003697fa
Paiva <danpaiva@live.com>
2022-04-04 22:51:29 -0300

    Add Infra codes

---
+++ Mercado.Prateleira/src/Mercado.Prateleira.Infrastruct.Data/DataRegistration/DataRegistration.cs
@@ -0,0 +1,21 @@
+﻿using Mercado.Prateleira.Infrastruct.Data.Contract;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mercado.Prateleira.Infrastruct.Data.DataRegistration
+{
+    public static class DataRegistration
+    {
+        public static IServiceCollection AddDataRegistration(
+            this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContextPool<DbContext, PrateleiraDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("sql_connection"));
+            });
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            return services;
+        }
+    }
+}
---
+++ Mercado.Prateleira/src/Mercado.Prateleira.Infrastruct.Data/PrateleiraDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Mercado.Prateleira.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mercado.Prateleira.Infrastruct.Data
+{
+    public class PrateleiraDbContext : DbContext
+    {
+        public PrateleiraDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Estoque> Estoques { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(PrateleiraDbContext).Assembly);
+        }
+    }
+}
